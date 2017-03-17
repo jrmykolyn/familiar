b21a5b6f6716baf95d7c29c8567ede6793cb4581
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-17 09:19:05 -0400

    Added 'man.js' file to project. File will display 'manual' info.

---
+++ man.js
@@ -0,0 +1,21 @@
+console.log( 'INSIDE `man.js`' );
+
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+const package = require( './package.json' );
+const args = process.argv.slice( 2 ) || [];
+
+
+// --------------------------------------------------
+// PARSE ARGS
+// --------------------------------------------------
+switch ( args[ 0 ] ) {
+    case '-v':
+    case '--version':
+        console.log( package.version );
+
+        break;
+    default:
+        console.log( 'Whoops! Looks like you ran the `familiar` command with missing or invalid arguments!' );
+}
