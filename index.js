cc2584422fa20cfd859fc609e1f450bd8b202949
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-19 23:03:34 -0400

    Started building out logic to parse/apply arguments received by 'gitf'.

--- index.js
+++ index.js
@@ -10,10 +10,66 @@ var main = require( './lib/main' );
 
 
 // --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+const PACKAGE_NAME = 'Familiar';
+const PACKAGE_COMMAND ='gitf';
+
+const args = process.argv.slice( 2 ) || [];
+const commandWhitelist = [ 'commit' ];
+var config = null;
+
+
+// --------------------------------------------------
+// DECLARE FUNCTIONS
+// --------------------------------------------------
+function parseArgs( args ) {
+    let command = args[ 0 ];
+
+    if ( !command ) {
+        printCommandError( 'MISSING_COMMAND' );
+        return; /// TEMP
+    }
+
+    if ( !validateCommand( command, commandWhitelist) ) {
+        console.log( `Whoops! The \`${command}\` command is not currently supported by ${PACKAGE_NAME}.`.error );
+        return; /// TEMP
+        /// TODO[@jrmykolyn] - Implement 'fallthrough' to native Git command(s).
+    }
+
+    applyCommand( command, args.slice( 1 ) );
+}
+
+function validateCommand( command, whitelist ) {
+    return whitelist.includes( command );
+}
+
+function applyCommand( command, args ) {
+    /// TODO[@jrmykolyn] - Build out logic for applying command(s).
+}
+
+function printCommandError( errorType ) {
+    errorType = errorType || '';
+
+    switch ( errorType ) {
+        case 'MISSING_COMMAND':
+            console.log( `Whoops! Looks like you called \`${PACKAGE_COMMAND}\` without a command.`.error );
+
+            break;
+        default:
+            console.log( 'Whoops! Something went wrong!'.error );
+    }
+}
+
+
+// --------------------------------------------------
 // INITIALIZE
 // --------------------------------------------------
 main.init()
     .then(
-        ( data ) => { console.log( JSON.parse( data ) ); },
+        ( data ) => {
+            config = data;
+            parseArgs( args );
+        },
         ( err ) => { console.log( err ); }
     );
