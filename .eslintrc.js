0730afb7dec1112215a9ac1849df408034a0e72f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-07-13 16:14:41 -0400

    chore(eslint) Fix formatting/normalize whitespace characters in '.eslintrc.js' file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-	"env": {
-		"browser": false,
-		"commonjs": true,
-		"es6": true,
-		"node": true,
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es6": true,
+        "node": true,
 
-	},
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"sourceType": "module",
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
         "ecmaVersion": 2017
-	},
-	"rules": {
-		"space-in-parens": [
-			"error",
-			"always"
-		],
-		"indent": [
-			"error",
-			4
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+    },
+    "rules": {
+        "space-in-parens": [
+            "error",
+            "always"
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
