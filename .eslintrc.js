fb9806343afe18c2cf438567d1305c225fdcf32a
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-07-13 09:37:27 -0400

    chore(eslint) Add 'eslintrc.js' file to repo.

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	"env": {
+		"browser": false,
+		"commonjs": true,
+		"es6": true,
+		"node": true,
+
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module",
+        "ecmaVersion": 2017
+	},
+	"rules": {
+		"space-in-parens": [
+			"error",
+			"always"
+		],
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
