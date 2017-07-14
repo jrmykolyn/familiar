e568b0e0b1767a6d4f1ad04801923d213a43a6f1
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-07-14 15:00:16 -0400

    Added test for 'familiar init' command. Add 'test/tmp/' dir. with 'README.md' file.

--- test/familiar.test.js
+++ test/familiar.test.js
@@ -3,6 +3,7 @@
 // --------------------------------------------------
 // Vendor
 import test from 'ava';
+import del from 'del';
 
 // Project
 import * as familiar from '../lib/familiar';
@@ -10,6 +11,10 @@ import * as familiar from '../lib/familiar';
 // --------------------------------------------------
 // DEFINE TESTS
 // --------------------------------------------------
+test.after( () => {
+    del( [ `${__dirname}/tmp/*`, `!${__dirname}/tmp/README.md` ] );
+} );
+
 test( 'Testing `familiar[ "-v" ]()`', ( t ) => {
     let semverPattern = /([A-z0-9]\.?){3}/gmi;
     let version = familiar[ '-v' ]( { silent: true } );
@@ -22,4 +27,11 @@ test( 'Testing `familiar[ "--version" ]()`', ( t ) => {
     let version = familiar[ '--version' ]( { silent: true } );
 
     t.truthy( version.match( semverPattern ) );
+} );
+
+test( 'Testing `familiar[ "init" ]()`', async ( t ) => {
+    // Write file to `./tmp/` dir.
+    let result = await familiar[ 'init' ]( { outDir: `${__dirname}/tmp` } );
+
+    t.true( result );
 } );
---
+++ test/tmp/README.md
@@ -0,0 +1,3 @@
+Do not remove! `README.md` file added so that `tmp/` dir. can be checked in to version control.
+
+Select tests write to `tmp/`, contents of directory are deleted when tests are complete.
